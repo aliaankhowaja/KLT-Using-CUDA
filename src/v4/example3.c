5dc4986a050109bf0c914984d99944a567d6fcac
MIsmail2524 <muhammadismaeel321@gmail.com>
2025-11-12 15:20:25 +0500

    Updated Timing with clock

--- src/v4/example3.c
+++ src/v4/example3.c
@@ -9,6 +9,7 @@ saved to a text file; each feature list is also written to a PPM file.
 #include <stdio.h>
 #include "pnmio.h"
 #include "klt.h"
+#include"time.h"
 
 /* #define REPLACE */
 
@@ -19,7 +20,8 @@ int main()
 #endif
 {
   unsigned char *img1, *img2;
-  char fnamein[100]; 
+  char fnamein[100];
+  char fnameout[100]; 
   KLT_TrackingContext tc;
   KLT_FeatureList fl;
   KLT_FeatureTable ft;
@@ -34,25 +36,35 @@ int main()
   tc->writeInternalImages = FALSE;
   tc->affineConsistencyCheck = -1; /* set this to 2 to turn on affine consistency check */
 
-  img1 = pgmReadFile("../../data/d0/img0.pgm", NULL, &ncols, &nrows);
+  img1 = pgmReadFile("../../data/d2/img0.pgm", NULL, &ncols, &nrows);
   img2 = (unsigned char *)malloc(ncols * nrows * sizeof(unsigned char));
 
   KLTSelectGoodFeatures(tc, img1, ncols, nrows, fl);
   KLTStoreFeatureList(fl, ft, 0);
   KLTWriteFeatureListToPPM(fl, img1, ncols, nrows, "feat0.ppm");
 
+  clock_t start_time, end_time;
+  double time_taken;
+
+  start_time = clock();  // Start timer
+
   for (i = 1; i < nFrames; i++)
   {
-    sprintf(fnamein, "../../data/d0/img%d.pgm", i);
+    sprintf(fnamein, "../../data/d2/img%d.pgm", i);
     pgmReadFile(fnamein, img2, &ncols, &nrows);
     KLTTrackFeatures(tc, img1, img2, ncols, nrows, fl);
 #ifdef REPLACE
     KLTReplaceLostFeatures(tc, img2, ncols, nrows, fl);
 #endif
     KLTStoreFeatureList(fl, ft, i);
-    // sprintf(fnameout, "feat%d.ppm", i);
-    // KLTWriteFeatureListToPPM(fl, img2, ncols, nrows, fnameout);
+     sprintf(fnameout, "feat%d.ppm", i);
+     KLTWriteFeatureListToPPM(fl, img2, ncols, nrows, fnameout);
   }
+  end_time = clock();  // End timer
+
+time_taken = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+printf("Total feature tracking time: %.3f seconds\n", time_taken);
+
   KLTWriteFeatureTable(ft, "features.txt", "%5.1f");
   KLTWriteFeatureTable(ft, "features.ft", NULL);
 
